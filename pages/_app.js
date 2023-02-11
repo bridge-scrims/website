a0eb250d2612f46a3daf84f81cfd41e1a80bf083
WhatCats <whatcater@gmail.com>
2023-02-11 15:49:33 +0100

    Changed GoogleFonts and Bootstrap to be downloaded serverside

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,35 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
+
+import { Poppins } from '@next/font/google'
+import { useEffect } from "react"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import '../styles/globals.css'
 
+const poppins = Poppins({
+  weight: ['200', '400', '500', '600', '800', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: "swap"
+})
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js")
+  })
+
   return (
-    <Layout>
-      <Head>
-        <meta charSet="UTF-8"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="color-scheme" content="dark light"></meta>
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <div id="__app" className={poppins.className}>
+      <Layout>
+        <Head>
+          <meta charSet="UTF-8"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="color-scheme" content="dark light"></meta>
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 }
