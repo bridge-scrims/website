46d24fa59dea8db90cc20b3cf6546b79433b3974
WhatCats <whatcater@gmail.com>
2023-03-12 16:30:33 +0100

    Added commonly used meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -24,9 +24,15 @@ export default function App({ Component, pageProps }) {
     <div id="__app" className={poppins.className}>
       <Layout>
         <Head>
-          <meta charSet="UTF-8"></meta>
+          <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="color-scheme" content="dark light"></meta>
+          <meta name="color-scheme" content="dark light" />
+          <meta name="title" content="Bridge Scrims" />
+          <meta name="description" content="Ready to take your Bridge game to the next level? Join Bridge Scrims, the largest Bridge community on Discord! Checkout our Minecraft server, participate in tournaments, make friends, and sharpen your skills. Join us now!" />
+          <meta name="keywords" content="Hypixel, The Bridge, community, tournaments, montages, texture packs, Minecraft, Discord" />
+          <meta property="og:title" content="Join Bridge Scrims, the largest Bridge community on Discord!" />
+          <meta property="og:description" content="Participate in tournaments, make friends, and sharpen your skills." />
+          <meta property="og:image" content="https://www.bridgescrims.net/shaders-day.png" />
         </Head>
         <Component {...pageProps} />
       </Layout>
