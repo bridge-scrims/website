e1f0e680d3311e254085ad42e19c55987c1b9294
WhatCats <whatcater@gmail.com>
2023-04-04 22:38:10 +0200

    Added custom 404 and 5xx responses

---
+++ src/pages/500.js
@@ -0,0 +1,23 @@
+import Head from "next/head"
+
+import LandingImageSection from "@/components/LandingImageSection"
+import styles from "@/styles/Fallback.module.css"
+import MainLayout from "@/components/MainLayout"
+
+export default function ServerError() {
+	return (
+		<>
+			<Head>
+				<title>Internal Server Error</title>
+			</Head>
+			<LandingImageSection id={styles.ErrorWrapper}>
+				<div className="imageTextWrapper">
+					<h1 id={styles.Error}>500 - Internal Server Error</h1>
+					<h2 id={styles.ErrorDetails}>Something went wrong. Please try again later.</h2>
+				</div>
+			</LandingImageSection>
+		</>
+	)
+}
+
+ServerError.Layout = MainLayout
