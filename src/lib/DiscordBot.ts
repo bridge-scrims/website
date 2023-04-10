19cef0fcc3e84da8e9d3c66d1b0ac449c36ffe37
WhatCats <whatcater@gmail.com>
2023-04-11 01:30:21 +0200

    Discord connection with discord.js

---
+++ src/lib/DiscordBot.ts
@@ -0,0 +1,35 @@
+import { Client, GatewayIntentBits, Guild } from "discord.js"
+
+const MAIN_GUILD = "759894401957888031"
+
+export type ScrimsClient = Client & { main: Guild }
+
+export const bot = new Client({
+    intents: GatewayIntentBits.GuildMembers,
+    shardCount: 1
+})
+
+let connectPromise: Promise<ScrimsClient | void> | null = null
+
+export async function getHostMember(memberId: string) {
+    const bot = await discordConnection()
+    return bot.main.members.cache.get(memberId)
+}
+
+export default async function discordConnection() {
+    if (!connectPromise) {
+        connectPromise = bot.login(process.env.DISCORD_TOKEN)
+            .then(() => console.log(`Connected to Discord as ${bot.user?.tag}`))
+            .then(() => bot.guilds.fetch(MAIN_GUILD))
+            .then(async (guild) => {
+                Object.defineProperty(bot, 'main', { value: guild })
+                await guild.roles.fetch()
+                await guild.members.fetch()
+                console.log(`Fetched ${guild.members.cache.size} members and ${guild.roles.cache.size} roles from host`)
+                return bot as ScrimsClient;
+            }).catch(console.error)
+    }
+    const connected = await connectPromise
+    if (!connected) throw new Error('Discord connection failed!')
+    return connected
+}
