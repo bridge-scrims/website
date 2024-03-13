712577f3b2821d9ac83f8cbc46e7d378d0eaef08
aMeow69 <152869470+aMeow69@users.noreply.github.com>
2024-03-13 12:03:48 +0100

    Update overlays.tsx
    Hopefully make the Overlays page function. If you press the big button that looks like a discord channel, it should redirect you to #bridge-overlays in the bridge scrims disc.

--- src/pages/overlays.tsx
+++ src/pages/overlays.tsx
@@ -1,23 +1,35 @@
+import Head from "next/head"
+
+import LandingImageSection from "@/components/LandingImageSection"
+import styles from "@/styles/Overlays.module.css"
 import MainLayout from "@/components/MainLayout"
-import { getVerifiedSession } from "@/lib/auth"
-import { GetServerSideProps } from "next"
 
-interface OverlaysProps {
-  admin: boolean
+function overlaysChannel() {
+  window.open("https://discord.com/channels/759894401957888031/809518867097190480");
 }
 
-export default function Overlays({ admin }: OverlaysProps) {
-  return <></>
-}
-
-export const getServerSideProps: GetServerSideProps<OverlaysProps> = async (context) => {
-  return { notFound: true }
-  const verified = await getVerifiedSession(context.req, context.res).catch(console.error)
-  return {
-    props: {
-      admin: !!verified
-    }
-  }
+export default function Overlays() {
+  return (
+    <>
+      <Head>
+        <title>Minecraft Server</title>
+      </Head>
+      <LandingImageSection>
+        <section id={styles.TitleSection} className="imageTextWrapper">
+          <h1 id={styles.Title}>{"Check out our dedicated Bridge Overlays channel in our Discord "}</h1>
+          <button
+            id={styles.ChannelButton}
+            type="button"
+            className="btn"
+            onClick={overlaysChannel}
+            title="Go To Channel"
+          >
+            # bridge-overlays
+          </button>
+        </section>
+      </LandingImageSection>
+    </>
+  )
 }
 
 Overlays.Layout = MainLayout
