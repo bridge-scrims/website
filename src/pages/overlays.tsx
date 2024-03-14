267ef93a060963426a23e36fe1e42625b9c4de03
WhatCats <whatcater@gmail.com>
2024-03-14 10:18:19 +0100

    update style of overlay channel link

--- src/pages/overlays.tsx
+++ src/pages/overlays.tsx
@@ -1,12 +1,8 @@
 import Head from "next/head"
 
 import LandingImageSection from "@/components/LandingImageSection"
-import styles from "@/styles/Overlays.module.css"
 import MainLayout from "@/components/MainLayout"
-
-function overlaysChannel() {
-  window.open("https://discord.com/channels/759894401957888031/809518867097190480");
-}
+import styles from "@/styles/Overlays.module.css"
 
 export default function Overlays() {
   return (
@@ -17,15 +13,15 @@ export default function Overlays() {
       <LandingImageSection>
         <section id={styles.TitleSection} className="imageTextWrapper">
           <h1 id={styles.Title}>{"Check out the dedicated Bridge Overlays channel in our Discord "}</h1>
-          <button
+          <a
             id={styles.ChannelButton}
-            type="button"
-            className="btn"
-            onClick={overlaysChannel}
+            target="_blank"
+            rel="noreferrer"
+            href="https://discord.com/channels/759894401957888031/809518867097190480"
             title="Go To Channel"
           >
             # bridge-overlays
-          </button>
+          </a>
         </section>
       </LandingImageSection>
     </>
